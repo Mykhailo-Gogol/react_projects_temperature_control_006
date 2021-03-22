632f5bd0c2b6511793017b50960a9b250510b895
Mykhailo-Gogol <68640759+Mykhailo-Gogol@users.noreply.github.com>
2021-03-22 18:08:41 +0200

    Додає стартовий код

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import "./App.css";
+import { useState } from "react";
+
+function App() {
+  const [temperatureValue, setTemperatureValue] = useState(10);
+  return (
+    <div className="app-container">
+      <div className="temperature-display-container">
+        <div className="temperature-display">{temperatureValue}°C</div>
+      </div>
+      <div className="button-container">
+        <button>+</button>
+        <button>-</button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
